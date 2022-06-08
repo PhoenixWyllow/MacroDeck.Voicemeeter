53c43798941e1c5818f4d8d6b7c8344746710866
Will <54237626+PhoenixWyllow@users.noreply.github.com>
2022-06-08 16:18:46 +0200

    fix blank devices when no label defined in VM

--- Models/VmIOInfo.cs
+++ Models/VmIOInfo.cs
@@ -4,12 +4,47 @@ using System.Text;
 
 namespace PW.VoicemeeterPlugin.Models
 {
-    public class VmIOInfo
+    public class VmIOInfo : IEquatable<VmIOInfo>
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public VmIOType Type { get; set; }
         public bool IsPhysical { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as VmIOInfo);
+        }
+
+        public bool Equals(VmIOInfo other)
+        {
+            return !(other is null) &&
+                   Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                return Id;
+            }
+            return string.Format("{0} \"{1}\"", Id, Name);
+        }
+
+        public static bool operator ==(VmIOInfo left, VmIOInfo right)
+        {
+            return EqualityComparer<VmIOInfo>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(VmIOInfo left, VmIOInfo right)
+        {
+            return !(left == right);
+        }
     }
 
     public enum VmIOType { Strip, Bus }
